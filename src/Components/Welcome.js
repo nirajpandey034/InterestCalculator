ac8cc8410ecfdfbd2f62e5bed14b4f23504bcbc2
Niraj Pandey <nirajpandey034@gmail.com>
2022-06-24 20:58:29 +0530

    some changes

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome() {
   let navigate = useNavigate();
   return (
     <div>
-      <div>Welcome To our Calculator world.</div>
+      <div>Welcome to our tool world.</div>
       {/* <button style={{cursor:'pointer'}} onClick={() => navigate("interest-calculator")}>
         Let's Calculate Interests
       </button> */}
